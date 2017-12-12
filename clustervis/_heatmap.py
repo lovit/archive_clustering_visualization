2cd11a4a24c1e66229d2f1361b73646405f3836f
Dongyoung <dongyoung.min@dm.snu.ac.kr>
2017-12-12 23:47:49 +0900

    Commit heatmap visualizer

---
+++ clustervis/_heatmap.py
@@ -0,0 +1,96 @@
+from sklearn.metrics import pairwise_distances
+import numpy as np
+import matplotlib.pyplot as plt
+
+def visualize_heatmap(centroids, metric='cosine', sort=None, segmentor=None, **kargs):    
+    n = centroids.shape[0]
+    pdist = pairwise_distances(centroids, metric=metric)
+    
+    if sort == 'row_sum':
+        pdist = _row_sum_sorting(pdist)
+
+    figure = _draw_figure(pdist, **kargs)
+    
+    if segmentor == 'reverse_band':        
+        points = _reversed_band_segmentation(pdist, **kargs)
+    
+    if segmentor and points and len(points) > 2:
+        plt.hold()
+        _plot_segmented_heatmap(pdist, points, kargs)
+    
+    return figure
+
+def _draw_figure(pdist, **kargs):
+    figsize = kargs.get('figsize', (15, 15))
+    dpi = kargs.get('dpi', 50)
+    facecolor = kargs.get('facecolor', None)
+    edgecolor = kargs.get('edgecolor', None)
+    frameon = kargs.get('frameon', True)    
+    title = kargs.get('title', None)
+    cmap = kargs.get('cmap', 'gray')
+    
+    n,m = pdist.shape
+    figure = plt.figure(figsize=figsize, dpi=dpi, facecolor=facecolor, edgecolor=edgecolor, frameon=frameon)
+    #ax = figure.add_subplot(1,1,1)
+    #ax.set_xlim([0, n])
+    #ax.set_ylim([0, m])
+    plt.imshow(pdist, cmap=cmap)
+    if title:
+        plt.title(title)
+        
+    return figure
+
+def _row_sum_sorting(pdist):
+    n = pdist.shape[0]    
+    sim_order = pdist.sum(axis=0).argsort()
+    
+    indices_orig = list(range(n))
+    indices_revised = np.ix_(sim_order,sim_order)
+
+    pdist_revised = np.empty_like(pdist)
+    pdist_revised[np.ix_(indices_orig,indices_orig)] = pdist[indices_revised]
+    return pdist_revised
+
+def _reversed_band_segmentation(pdist, **kargs):
+    def reversed_band_matrix(n, k):
+        A = np.zeros((n,n))
+        for i in range(-k,k):
+            A += np.eye(n,k=i)
+        A = np.flip(A,0)
+        return A
+    
+    n = kargs.get('reverse_band_filter_width', int(pdist.shape[0]*0.5))
+    k = kargs.get('reverse_band_band_width', min(int(n/2), 10))
+    
+    A = reversed_band_matrix(n, k)
+    means = []
+    for i in range(1,pdist.shape[0]-n):
+        block = pdist[i+0:i+n,i+0:i+n]
+        means.append(np.mean(A * block))
+
+    tangents = []
+    for ix in range(1, len(means)):
+        tangents.append(means[ix]-means[ix-1])
+
+    points = []
+    for ix, val in enumerate(tangents[:-1]):
+        if val < 0:
+            pass
+        if val*tangents[ix+1] < 0:
+            points.append(ix+n//2)
+            
+    return [0] + points + [pdist.shape[0]]
+
+def _plot_segmented_heatmap(pdist, points, kargs):
+    def add_lines(k, xmin, xmax):
+        plt.hlines(k, xmin, xmax, colors=line_color, linewidth=line_width)
+        plt.vlines(k, xmin, xmax, colors=line_color, linewidth=line_width)
+    
+    line_width = kargs.get('line_width', 3)    
+    line_color = kargs.get('boundary_line_color', 'red')
+        
+    for ix, point in enumerate(points):
+        if ix == 0 or ix == len(points)-1:
+            continue
+        add_lines(point, points[ix-1], point)
+        add_lines(point, point, points[ix+1])
